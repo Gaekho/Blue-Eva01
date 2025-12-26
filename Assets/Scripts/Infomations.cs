7387c01a30145dfaddca50e6d91a3ab68cedf60a
sevan94 <evanseo888@gmail.com>
2025-12-26 19:09:32 +0900

    정보 저장용 scriptable object 틀 생성

---
+++ Assets/Scripts/Infomations.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InteractInfomation", menuName = "Blue Eva01/Investigation/Infomation")]
+public class Infomations : ScriptableObject
+{
+
+}
