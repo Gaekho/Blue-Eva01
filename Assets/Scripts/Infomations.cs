4345b1ce85428942f4a26e36148ef3f6b5ea809a
sevan94 <evanseo888@gmail.com>
2025-12-26 20:01:25 +0900

    메모 관리할 scriptable object생성, 메모와 정보 관리할 scriptable object생성

--- Assets/Scripts/Infomations.cs
+++ Assets/Scripts/Infomations.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum InfoType
+{
+    important,
+    trash
+}
+
 [CreateAssetMenu(fileName = "InteractInfomation", menuName = "Blue Eva01/Investigation/Infomation")]
 public class Infomations : ScriptableObject
 {
-
+    [Header("Investigation Inform")]
+    [SerializeField] public InfoType type;
+    [SerializeField] private int index;
+    [SerializeField] private TextAsset infoDetail;
+    [SerializeField] public MemoType memoName;
 }
